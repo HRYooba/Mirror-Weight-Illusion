b489fe775cdf18215d601b4dfdb8a83dfa2b5577
大場洋哉 <HRYooba@gmail.com>
2018-01-13 23:51:24 +0900

    update experiment2

--- Assets/Work/Scripts/Experiment2SystemManager.cs
+++ Assets/Work/Scripts/Experiment2SystemManager.cs
@@ -118,6 +118,7 @@ public class Experiment2SystemManager : MonoBehaviour
         else
         {
             moveCount = 0;
+            writeState = state.WAIT;
         }
 
         // MirrorHand and TrackerHand switch active
